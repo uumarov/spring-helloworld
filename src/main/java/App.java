df9d9a456c4fcb61fe7607a49855fb5b4f1a46bd
Usman Umarov <usman@umarov.ru>
2021-01-27 18:40:02 +0300

    2.1.2 v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
+
     }
 }
